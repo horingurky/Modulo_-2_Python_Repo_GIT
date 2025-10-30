35a7bbec0a10a9c9fe5bb6ad7fec44f8b40a5aae
Marcos <129058792+horingurky@users.noreply.github.com>
2025-10-30 12:20:26 +0100

    ejemplo post

---
+++ 03_pydantic/ejemplo_post.py
@@ -0,0 +1,33 @@
+
+# Teclea en consola uvicorn 03_pydantic.ejemplo_post:app --reload
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+# modelo básico
+class Usuario(BaseModel):
+    nombre: str
+    edad: int
+    activo: bool
+
+# modelo con campos opcionales y valores por defecto
+class Producto(BaseModel):
+    nombre: str
+    precio: float
+    descripcion: Optional[str] = None
+    disponible: bool = True
+
+@app.post("/usuarios")
+def crear_usuario(usuario: Usuario):
+    return {
+    "mensaje": f"Usuario {usuario.nombre} creado correctamente"
+    }
+
+@app.post("/productos")
+def crear_producto(producto: Producto):
+    return {
+    "mensaje": f"Producto {producto.nombre} creado correctamente"
+    }
